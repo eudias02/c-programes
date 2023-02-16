c07b7092b4a22ec487ffe00e47e921241ee9c60e
eudias02 <122534596+eudias02@users.noreply.github.com>
2023-02-16 14:47:40 +0300

    Add files via upload

---
+++ grading systems.c
@@ -0,0 +1,49 @@
+//grading system
+
+#include<stdio.h>
+
+int main()
+
+{
+
+int maths,chem,sci,average;
+
+printf("enter marks for maths,chem,sci:");
+
+scanf("%d%d%d",&maths,&chem,&sci);
+
+average=(maths+chem+sci)/3;
+
+if(average>=70 && average<=100){
+ 
+printf("A");
+   
+ }
+
+ else if(average>=60 && average <=69){
+ 
+   printf("B");
+
+}
+ 
+else if(average>=50 && average <=59){
+
+printf("C");
+
+         }
+
+ else if (average>=40 && average <=49){
+
+    printf("D");
+
+}
+
+ else if(average>=0 && average <=39){
+
+    printf("E");
+
+}
+
+return 0;
+
+}
