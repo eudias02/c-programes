a1c69c61be4de6ff591160ab3de65d594f01dced
eudias02 <122534596+eudias02@users.noreply.github.com>
2023-02-23 14:07:50 +0300

    Add files via upload

---
+++ natural 100 numbers.c
@@ -0,0 +1,12 @@
+ //natural numbers
+ #include<stdio.h>
+ int main()
+ {
+   int i;
+   for(i=100; i>=0; i--)
+   {
+       printf("%d ",i);
+   }
+   printf("for loop body finished");
+   return 0;
+ }
